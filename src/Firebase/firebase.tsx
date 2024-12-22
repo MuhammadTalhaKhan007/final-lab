620b3e6f04c1756e1c0374ab2ea470734e293a4f
Muhammad Talha Khan <139450184+MuhammadTalhaKhan007@users.noreply.github.com>
2024-12-23 03:01:06 +0500

    Updated babel and everything

--- src/Firebase/firebase.tsx
+++ src/Firebase/firebase.tsx
@@ -5,15 +5,18 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import {
-  EXPO_FIREBASE_API_KEY,
-  EXPO_FIREBASE_AUTH_DOMAIN,
-  EXPO_FIREBASE_PROJECT_ID,
-  EXPO_FIREBASE_STORAGE_BUCKET,
-  EXPO_FIREBASE_MESSAGING_SENDER_ID,
-  EXPO_FIREBASE_APP_ID,
-  EXPO_FIREBASE_MEASUREMENT_ID,
-} from "@env";
+
+const EXPO_FIREBASE_API_KEY = process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+const EXPO_FIREBASE_AUTH_DOMAIN = process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const EXPO_FIREBASE_PROJECT_ID = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID;
+const EXPO_FIREBASE_STORAGE_BUCKET =
+  process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const EXPO_FIREBASE_MESSAGING_SENDER_ID =
+  process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const EXPO_FIREBASE_APP_ID = process.env.EXPO_PUBLIC_FIREBASE_APP_ID;
+const EXPO_FIREBASE_MEASUREMENT_ID =
+  process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID;
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
